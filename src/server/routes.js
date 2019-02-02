6f5640413ccaec9ccbb2347f1c340dfd7eb5c85e
David Silva <dsilva917@gmail.com>
2019-02-02 15:37:49 -0500

    Add a route for receiving the actual nucleotide data

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { promises: fs } = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 
 module.exports = (router = new Router()) => {
@@ -10,15 +10,36 @@ module.exports = (router = new Router()) => {
   router.get('/sequence/:name', async (req, res) => {
     const name = req.params.name;
     const filepath = path.join('..', '..', 'data', name);
-    const stats = await fs.stat(path.join(filepath, 'nucleotides'));
-    const annotations = await fs.readFile(path.join(filepath, 'annotations.json'));
-    if (stats && annotations) {
+    try {
+      const stats = await fs.stat(path.join(filepath, 'nucleotides'));
+      const annotations = await fs.readFile(path.join(filepath, 'annotations.json'));
       const data = {
         nucleotides: stats.size,
         annotations: JSON.parse(annotations)
       };
       res.json(data);
-    } else {
+    } catch(e) {
+      const error = { error: `Sequence ${name} not found` };
+      res.status(404).json(error);
+    }
+  });
+
+  router.get('/sequence/:name/nucleotides', async (req, res) => {
+    const name = req.params.name;
+    const filepath = path.join('..', '..', 'data', name);
+    const start = Number(req.query.start || 0);
+    const end = Number(req.query.end || 1000);
+    const length = end - start;
+    try {
+      const file = await fs.open(path.join(filepath, 'nucleotides'), 'r');
+      const { buffer, bytesRead } =
+            await fs.read(file, Buffer.alloc(length), 0, length, start);
+      res.json({
+        nucleotides: buffer.toString('utf8', 0, bytesRead),
+        start,
+        end: start + bytesRead
+      });
+    } catch(e) {
       const error = { error: `Sequence ${name} not found` };
       res.status(404).json(error);
     }
--- src/server/routes.test.js
+++ src/server/routes.test.js
@@ -1,14 +1,9 @@
 const routes = require('./routes');
 const express = require('express');
 const request = require('supertest');
-const fs = require('fs');
+const fs = require('fs-extra');
 
-jest.mock('fs', () => ({
-  promises: {
-    stat: jest.fn(),
-    readFile: jest.fn()
-  }
-}));
+jest.mock('fs-extra');
 
 const initServer = () => {
   const app = express();
@@ -26,17 +21,18 @@ describe('GET /', () => {
 
 describe('GET /sequence/:name', () => {
   it('returns a 404 if the sequence is not found', async () => {
-    fs.promises.stat.mockResolvedValue(undefined);
-    fs.promises.readFile.mockResolvedValue(undefined);
+    fs.stat.mockRejectedValue({});
+    fs.readFile.mockRejectedValue({});
 
     const app = initServer();
     const response = await request(app).get('/sequence/non_existent');
     expect(response.status).toBe(404);
+    expect(response.body.error).toBe('Sequence non_existent not found');
   });
 
   it('returns data about the sequence', async () => {
-    fs.promises.stat.mockResolvedValue({ size: 10 });
-    fs.promises.readFile.mockResolvedValue(JSON.stringify([
+    fs.stat.mockResolvedValue({ size: 10 });
+    fs.readFile.mockResolvedValue(JSON.stringify([
       {start: 0, end: 9, name: 'The whole thing', strand: '+'}
     ]));
 
@@ -45,5 +41,48 @@ describe('GET /sequence/:name', () => {
     expect(response.status).toBe(200);
     expect(response.body.nucleotides).toBe(10);
     expect(response.body.annotations[0].start).toBe(0);
+  });
+});
+
+describe('GET /sequence/:name/nucleotides', () => {
+  it('returns a 404 if the sequence is not found', async () => {
+    fs.readFile.mockRejectedValue({});
+
+    const app = initServer();
+    const response = await request(app).get('/sequence/non_existent/nucleotides');
+    expect(response.status).toBe(404);
+    expect(response.body.error).toBe('Sequence non_existent not found');
+  });
+
+  it('returns nucleotide data', async () => {
+    fs.open.mockResolvedValue(1);
+    fs.read.mockResolvedValue({
+      buffer: Buffer.from('actggactgg'),
+      bytesRead: 10
+    });
+
+    const app = initServer();
+    const response = await request(app).get('/sequence/test/nucleotides');
+    expect(response.status).toBe(200);
+    expect(fs.read).toHaveBeenCalledWith(1, expect.any(Buffer), 0, 1000, 0);
+    expect(response.body.nucleotides).toBe('actggactgg');
+    expect(response.body.start).toBe(0);
+    expect(response.body.end).toBe(10);
+  });
+
+  it('returns nucleotide data starting from a given offset', async () => {
+    fs.open.mockResolvedValue(1);
+    fs.read.mockResolvedValue({
+      buffer: Buffer.from('actggactgg'),
+      bytesRead: 10
+    });
+
+    const app = initServer();
+    const response = await request(app).get('/sequence/test/nucleotides').query({ start: 10, end: 20 });
+    expect(response.status).toBe(200);
+    expect(fs.read).toHaveBeenCalledWith(1, expect.any(Buffer), 0, 10, 10);
+    expect(response.body.nucleotides).toBe('actggactgg');
+    expect(response.body.start).toBe(10);
+    expect(response.body.end).toBe(20);
   });
 });
